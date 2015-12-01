555533fba827902cc81f6edf20d472f39473247b
Franzé Jr <franzejr@gmail.com>
2015-12-01 12:39:37 -0300

    Mention the ruby version

--- tricks/source_code/group_using_itself.rb
+++ tricks/source_code/group_using_itself.rb
@@ -1,6 +1,7 @@
 
-## Using the Itself method http://ruby-doc.org/core-2.2.0/Object.html#method-i-itself
+## Using the Itself method in Ruby 2.2
 ## we can group elements from one array
+## Reference: http://ruby-doc.org/core-2.2.0/Object.html#method-i-itself
 
 [2, 3, 3, 1, 2, 3, 3, 1, 1, 2].group_by(&:itself)
 
